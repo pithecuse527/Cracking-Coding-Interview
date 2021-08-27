b473b958398c30a9475f544c8acd5f6d926472e9
pithecuse527 <gihong96@gmail.com>
2021-08-27 21:12:02 +0900

    add iterative solution

--- ch2/kth_element.cpp
+++ ch2/kth_element.cpp
@@ -28,7 +28,7 @@ public:
 template<class T>
 class Solution {
 public:
-  Node<T>* solve(Node<T>* node, int k) {
+  Node<T>* solve(Node<T>* node, int k) {  // recursive solution
     static int i = 0;   // shared by every recursive call
     if (node == NULL) return NULL;
 
@@ -38,6 +38,21 @@ public:
       return node;
     }
     return to_return;
+  }
+
+  Node<T>* solve2(Node<T>* node, int k) { // iterative solution
+    Node<T>* runner1 = node;
+    Node<T>* runner2 = node;
+
+    // move runner2 for k so that runner1 and runner2 has a distance k
+    for (int i=0; i<k; i++) runner2 = runner2->getNext();
+
+    while(runner2 && runner2->getNext()) { // until runner2 reaches to the end
+      runner1 = runner1->getNext(); // hop together
+      runner2 = runner2->getNext();
+    }
+
+    return runner1;
   }
 
 };
