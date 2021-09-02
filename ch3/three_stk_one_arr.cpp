9ecc0fd5148281082c2a9c98c9217e189bd1708c
pithecuse527 <gihong96@gmail.com>
2021-09-02 11:55:55 +0900

    implement three stacks on an array

---
+++ ch3/three_stk_one_arr.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <iostream>
+#include <cmath>
+#define STK_SIZE 10
+
+using namespace std;
+
+class ThreeStk {
+public:
+  int buffer[STK_SIZE*3];
+  int stack_headers[3] = {-1, -1, -1};
+
+  bool push(int stk_num, int val) {
+    if (stack_headers[stk_num] + 1 > STK_SIZE) return false;
+
+    stack_headers[stk_num]++;
+    buffer[topOfStack(stk_num)] = val;
+  }
+
+  bool pop(int stk_num) {
+    if (stack_headers[stk_num] == -1) return false;
+    buffer[topOfStack(stk_num)] = 0;
+    stack_headers[stk_num]--;
+    return true;
+  }
+
+  int topOfStack(int stk_num) {
+    return stk_num*STK_SIZE+stack_headers[stk_num]
+  }
+
+};
