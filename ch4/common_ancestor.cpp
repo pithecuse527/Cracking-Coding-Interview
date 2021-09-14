cf002e1bdfdf78e417d5e780c75e1ea85f6cf9d5
pithecuse527 <gihong96@gmail.com>
2021-09-15 07:46:41 +0900

    second version (without accessing to the parent node)

--- ch4/common_ancestor.cpp
+++ ch4/common_ancestor.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-// when we have an access to the parent node
+// when we have an access to the parent node (bottom-up)
 Node* firstVersion(Node* root, Node* n1, Node* n2) {
   Node* runner1;  // first runner going up to the root
   Node* runner2;  // second runner going up to the root
@@ -25,9 +25,36 @@ Node* firstVersion(Node* root, Node* n1, Node* n2) {
 
 }
 
-// Node* secondVersion() { // when we do not have an access to the parent node
-//
-// }
+bool nodeInSubtree(Node* subTreeRoot, Node* to_find) {
+  if (!subTreeRoot) return false; // have not found
+  if (subTreeRoot == to_find) return true;
+  bool exist_on_left = nodeInSubtree(subTreeRoot->getLeft(), to_find);
+  bool exist_on_right = nodeInSubtree(subTreeRoot->getRight(), to_find);
+
+  if (exist_on_left || exist_on_right) return true;
+  return false;
+
+}
+
+// when we do not have an access to the parent node (top-down)
+// we also assume the n1 and n2 always exist in the tree
+Node* secondVersion(Node* root, Node* n1, Node* n2) {
+  Node* common_ancestor_ = root;
+  bool n1_in_left = nodeInSubtree(common_ancestor_->getLeft(), n1);
+  bool n2_in_left = nodeInSubtree(common_ancestor_->getLeft(), n2);
+
+  while(common_ancestor_ && n1_in_left && n2_in_left) {
+    if (n1_in_left && n2_in_left) common_ancestor_ = common_ancestor_->getLeft();
+    if (!n1_in_left && !n2_in_left) common_ancestor_ = common_ancestor_->getRight();
+
+    n1_in_left = nodeInSubtree(common_ancestor_->getLeft(), n1);
+    n2_in_left = nodeInSubtree(common_ancestor_->getLeft(), n2);
+
+  }
+
+  // as we assume n1 and n2 is in the tree, this won't happen
+  return common_ancestor_;
+}
 
 int main() {
   Node n1(1);
@@ -51,9 +78,11 @@ int main() {
   n5.setParent(&n4);
 
 
-  Node* common_ancestor_ = firstVersion(&n1, &n5, &n7);
+  Node* common_ancestor_1 = firstVersion(&n1, &n5, &n7);
+  Node* common_ancestor_2 = secondVersion(&n1, &n5, &n7);
 
-  cout << common_ancestor_->getVal() << endl;
+  cout << common_ancestor_1->getVal() << endl;
+  cout << common_ancestor_2->getVal() << endl;
 
   return 0;
 }
