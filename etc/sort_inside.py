b142d4e2f7f216f6d9fee14ed7dbf2db1c1e5413
pithecuse527 <gihong96@gmail.com>
2022-01-05 11:55:43 +0900

    add second version with bucket sort

--- etc/sort_inside.py
+++ etc/sort_inside.py
@@ -1,6 +1,8 @@
 import heapq
+num_ori = int(input())
 
-num = int(input())
+# first version O(nlgn)
+num = num_ori
 heap = []
 while num > 0:
     heapq.heappush(heap, num % 10)
@@ -12,3 +14,16 @@ while heap:
     result += heapq.heappop(heap) * mul
     mul *= 10
 print(result)
+
+# second version O(n)
+bucket = [0 for _ in range(10)]
+num = num_ori
+while num:
+    bucket[num % 10] += 1
+    num //= 10
+
+for i in range(9, -1, -1):
+    while bucket[i] > 0:
+        print(str(i), end='')
+        bucket[i] -= 1
+print()
