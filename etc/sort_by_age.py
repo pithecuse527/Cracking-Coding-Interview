15e497e0424123dc5567b5de706d8600c6b196f9
pithecuse527 <gihong96@gmail.com>
2022-01-05 12:27:41 +0900

    sort using custom key

---
+++ etc/sort_by_age.py
@@ -0,0 +1,10 @@
+N = int(input())
+
+lst = []
+for _ in range(N):
+    age, name = input().split()
+    lst.append((int(age), name))
+lst.sort(key=lambda x:x[0])
+
+for i in lst:
+    print(i[0], i[1])
