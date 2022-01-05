3891a6e37ebd089311ab9caa0bbbedf973e9bbb6
pithecuse527 <gihong96@gmail.com>
2022-01-05 12:29:32 +0900

    add comment about the stable

--- etc/sort_by_age.py
+++ etc/sort_by_age.py
@@ -4,7 +4,9 @@ lst = []
 for _ in range(N):
     age, name = input().split()
     lst.append((int(age), name))
-lst.sort(key=lambda x:x[0])
+
+# compare only with the first element, others will be stable
+lst.sort(key=lambda x:x[0]) 
 
 for i in lst:
     print(i[0], i[1])
