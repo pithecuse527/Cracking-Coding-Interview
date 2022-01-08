0602de04c265dfdbcc908595f76d34686e444fc2
pithecuse527 <gihong96@gmail.com>
2022-01-08 11:02:42 +0900

    much better algorithm with substring

--- etc/doc_search.py
+++ etc/doc_search.py
@@ -4,18 +4,11 @@ word = input()
 runner = 0
 answer = 0
 while runner <= len(docs)-len(word):
-    found = True
-    last_loc = runner
-    for w in word:
-        if docs[runner] != w:
-            found = False
-            break;
-        runner += 1
-        
-    if found:
+    if docs[runner:runner+len(word)] == word:
         answer += 1
+        runner += len(word)
     else:
-        runner = last_loc+1
+        runner += 1
     
 print(answer)
         
