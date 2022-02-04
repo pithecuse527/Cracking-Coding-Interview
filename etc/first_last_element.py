7998fc38652240d1f84c83762f9a711833efb81b
pithecuse527 <gihong96@gmail.com>
2022-02-04 12:13:20 +0900

    more faster version with two binary search

--- etc/first_last_element.py
+++ etc/first_last_element.py
@@ -1,24 +1,48 @@
 class Solution:
-    def searchRange(self, nums, target):
+    def findStart(self, nums, target):
         left = 0
         right = len(nums)-1
-        
-        while left <= right:
-            mid = (left + right) // 2
-            if target == nums[mid]:
-                runner1 = mid
-                while runner1 >= 0 and target == nums[runner1]:
-                    runner1 -= 1
-                runner2 = mid
-                while runner2 <= len(nums)-1 and target == nums[runner2]:
-                    runner2 += 1
-                return [runner1+1, runner2-1]
-            elif target < nums[mid]:
-                right = mid - 1
-            else:
-                left = mid + 1
+        if nums[left] == target:
+            return left
 
-        return [-1, -1]
+        while left <= right:
+            mid = (left+right)//2
+            if target == nums[mid] and target != nums[mid-1]:
+                return mid
+            elif target > nums[mid]:
+                left = mid+1
+            else:
+                right = mid-1
+        return -1
+    
+    def findEnd(self, nums, target):
+        left = 0
+        right = len(nums)-1
+        if nums[right] == target:
+            return right
+
+        while left <= right:
+            mid = (left+right)//2
+            if target == nums[mid] and target != nums[mid+1]:
+                return mid
+            elif target < nums[mid]:
+                right = mid-1
+            else:
+                left = mid+1
+        return -1
+
+    def searchRange(self, nums, target):
+        # base case
+        if len(nums) == 0:
+            return [-1, -1]
+
+        # find the starting point
+        start = self.findStart(nums, target)
+
+        # find the end point
+        end = self.findEnd(nums, target)
+
+        return [start, end]
                 
 s = Solution()
 print(s.searchRange([1], 1))
