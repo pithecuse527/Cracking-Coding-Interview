3576089a4cc662dec9efff0dd7aa23f7dc404915
pithecuse527 <gihong96@gmail.com>
2022-02-04 17:03:51 +0900

    solve symmetric tree problem with recursion

---
+++ etc/symmetric_tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def check(left, right):
+            if left == None and right == None:
+                return True
+            if left == None or right == None:
+                return False
+            if left.val != right.val:
+                return False
+            else:
+                return check(left.left, right.right) and check(left.right, right.left)
+        return check(root, root)
