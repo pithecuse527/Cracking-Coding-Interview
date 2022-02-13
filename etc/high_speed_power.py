27817e62be5221c395a55906768cb078317d0bde
pithecuse527 <gihong96@gmail.com>
2022-02-13 19:07:15 +0900

    remove needless modular operations

--- etc/high_speed_power.py
+++ etc/high_speed_power.py
@@ -4,9 +4,9 @@ def mult(a, b, c):
     if b == 0:
         return 1
     if b == 1:
-        return a%c
+        return a
     if b == 2:
-        return (a*a)%c
+        return (a*a)
     
     remainder = mult(a, b%2, c)
     half = mult(a, b//2, c)
