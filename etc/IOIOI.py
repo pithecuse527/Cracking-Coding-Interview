f5881b0fcb47374810789894779754500e63bccc
pithecuse527 <gihong96@gmail.com>
2022-03-01 22:08:36 +0900

    string and hash problem

---
+++ etc/IOIOI.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+M = int(input())
+str = input()
+
+runner = 1
+ans = 0
+sub_IOI = 0
+while runner < M:
+    if str[runner-1]=='I' and str[runner]=='O' and str[runner+1]=='I':
+        sub_IOI += 1
+        if sub_IOI == N:
+            sub_IOI -= 1
+            ans += 1
+        runner = runner + 2
+    else:
+        sub_IOI = 0
+        runner += 1
+print(ans)
