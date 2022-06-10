73118e5e6734f2ee1acf3ee14aa5efa1f12bcca4
pithecuse527 <gihong96@gmail.com>
2022-06-10 16:59:09 +0900

    use two dfs

--- etc/start_and_link.py
+++ etc/start_and_link.py
@@ -1,32 +1,26 @@
 import sys
 input = sys.stdin.readline
 N = int(input())
-INF = 1e9
+visited = [[False]*N for _ in range(N)]
 mtx = [list(map(int, input().split())) for _ in range(N)]
-people = set([x for x in range(N)])
-min_diff = INF
+min_val = 1e9
 
-def scoring(team, team_size):
-    score = 0
-    for i in range(team_size-1):
-        for j in range(i+1, team_size):
-            score += mtx[team[i]][team[j]]
-            score += mtx[team[j]][team[i]]
-    return score
-
-def dfs(start_team, team_size, pvt):
-    global min_diff
-    if team_size == N // 2:
-        start_team_score = scoring(list(start_team), team_size)
-        link_team = people.difference(start_team)
-        link_team_score = scoring(list(link_team), team_size)
-        min_diff = min(min_diff, abs(start_team_score-link_team_score))
+def dfs(depth):
+    global min_val
+    if depth == N:
+        score1, score2 = 0, 0
+        for i in range(N-1):
+            for j in range(i+1, N):
+                if visited[i] and visited[j]:
+                    score1 += mtx[i][j]+mtx[j][i]
+                elif not visited[i] and not visited[j]:
+                    score2 += mtx[i][j]+mtx[j][i]
+        min_val = min(min_val, abs(score1-score2))
         return
     
-    for i in range(pvt, N):
-        start_team.append(i)
-        dfs(start_team, team_size+1, i+1)
-        start_team.pop()
-    
-dfs([], 0, 0)
-print(min_diff)
+    visited[depth] = True
+    dfs(depth+1)
+    visited[depth] = False
+    dfs(depth+1)
+dfs(0)
+print(min_val)
