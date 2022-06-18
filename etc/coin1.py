0aa730b3557f5182a0c090f857d328c92ca4445c
pithecuse527 <gihong96@gmail.com>
2022-06-18 20:06:42 +0900

    make the money amount with dp

---
+++ etc/coin1.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+N, K = map(int, input().split())
+coins = [int(input()) for _ in range(N)]
+dp = [0] * (K+1)
+dp[0] = 1
+
+for coin in coins:
+    for i in range(coin, K+1):
+        dp[i] += dp[i-coin]
+print(dp[-1])
